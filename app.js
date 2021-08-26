61b7653ca7a001e304f3763c4858d09abfef87f7
Spencer Pequegnat <spencer.peq@gmail.com>
2021-08-25 18:37:04 -0700

    Delete items

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const item2 = new Item({
   name: "Hit the + button to add a new item."
 })
 const item3 = new Item({
-  name: "<--- Hit this to mark an item as complete."
+  name: "<--- Hit this to delete an item."
 })
 
 const defaultItems = [item1, item2, item3]
@@ -60,6 +60,18 @@ app.post("/", function(req, res){
   item.save();
   res.redirect("/");
 });
+
+app.post("/delete", function(req, res){
+  const checkedItemId = req.body.checkbox
+  Item.findByIdAndRemove(checkedItemId, function(err){
+    if(err){
+      console.log(err)
+    } else {
+      console.log("Successfully deleted.")
+    }
+  })
+  res.redirect("/")
+})
 
 app.get("/work", function(req,res){
   res.render("list", {listTitle: "Work List", newListItems: workItems});
